90f374b56c5ff66e98dbca014266283f1cf52ed8
Rory Hofland <roryhofland@gmail.com>
2021-09-27 14:40:41 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+
+const server = express();
+const port = 3000;
+
+//Middleware
+server.set("views", path.join(__dirname, "views"));
+server.set("view engine", "ejs");
+
+server.use(express.urlencoded({ extended: false}));
+server.use(express.json())
+server.use(express.static(path.join(__dirname, "public")));
+
+server.get("/", (req, res) => {
+    res.render("index");
+});
+
+server.get("/contact", (req, res) => {
+    res.render("contact");
+});
+
+server.get("/resume", (req, res) => {
+    res.render("resume");
+});
+
+server.get("/projects", (req, res) => {
+    res.render("projects");
+});
+
+server.post("/send", (req, res) => {
+    res.json({ message: "Message received."})
+})
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+});
